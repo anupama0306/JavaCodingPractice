2c0e67c25ba132a6a6087ea4d9588253a952e7be
anupama0306 <anupama.joshi88@gmail.com>
2024-05-27 22:45:18 +0530

    Added first class

--- src/main/java/LambdaFunctions.java
+++ src/main/java/LambdaFunctions.java
@@ -1,2 +1,10 @@
 public class LambdaFunctions {
+
+    public static void main(String[] args){
+        useLambda((x,y) -> {System.out.println("Work done"+x+y);});
+    }
+
+    public static void useLambda(Workable w){
+        w.work(3,5);
+    }
 }
---
+++ src/main/java/Workable.java
@@ -0,0 +1,5 @@
+@FunctionalInterface
+public interface Workable {
+    public void work(int a, int b);
+
+}
